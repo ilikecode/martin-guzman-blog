f6ef313caa921637af98257420029e4c4ae4343b
Chris Ibbitson <ilikecode@users.noreply.github.com>
2021-05-24 16:58:54 -0700

    Add scroll update position check. Remove unused prism css files.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,36 @@
 /* 	Import ThemeProvider, GlobalStyles logic from root-wrapper.js.
 		Export to wrap all pages */
-
 import 'prismjs/plugins/command-line/prism-command-line.css'
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
-import 'prismjs/plugins/line-highlight/prism-line-highlight.css'
-
-
-import 'prismjs/themes/prism-solarizedlight.css';
-import 'prismjs/themes/prism-okaidia.css'
 import 'prismjs/themes/prism.css'
 import { wrapRootElement as wrap } from "./root-wrapper"
 
 require("prismjs/themes/prism.css");
 
+export const wrapRootElement = wrap;
 
+//Add Scroll Update Position Check
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+  }) => {
+   // transition duration from `layout.js` * 1000 to get time in ms
+  const TRANSITION_DELAY = 0.3 * 1000 * 2
+ 
+  // if it's a "normal" route
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+  }
+ 
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0]
+ 
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+  }
+ 
+  return false
+};
 
-export const wrapRootElement = wrap
 
 
 
