fc2676ca33605aa77746021f4a13f9b6dbb7bca2
Chris Ibbitson <ilikecode@users.noreply.github.com>
2021-05-24 18:49:25 -0700

    Remove unused styled-component.

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,7 +7,6 @@ import React from 'react'
 // Import Utilities and Style Elements
 import { 
 	Container, 
-	CopyIntroInternalLink, 
 	HeroParagraph 
 } from '../../elements/'
 
@@ -15,7 +14,7 @@ const Hero = () => {
   return (
     <Container>
 			<h1>Hello, I'm Martin.</h1>
-			<HeroParagraph>Originally from Southern California, now living in Vancouver, BC, and I'm currently studying for my OSCP Certification.</HeroParagraph> 
+      <HeroParagraph>Originally from Southern California, now living in Vancouver, BC, and I'm currently studying for my OSCP Certification.</HeroParagraph>
       <HeroParagraph>A self-starter with a strong analytical mindset and a friendly personality. Skills in EDR/EPP, penetration testing, open-source intelligence, and scripting. Experienced with multiple OS platforms including Linux, Windows, Mac, IOS, Android, and more. Constantly learning new cybersecurity skills and researching the latest technology. Also, a top-ranked National Cyber League competitor who loves the challenge of testing my skillset against others and continually improving my knowledge and rankings.</HeroParagraph>
     </Container>
   )
